e4b3326d9e24f9d9a966738036d1be8c0a33a2da
Anastasia Simanova <frerarj@gmail.com>
2023-05-09 15:52:31 +0300

    Refactor brain-even-game.js

--- src/games/brain-even-game.js
+++ src/games/brain-even-game.js
@@ -3,16 +3,17 @@ import getRandomNumber from '../utils/getRandomNumber.js';
 
 const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    return 'yes';
+  }
+  return 'no';
+};
+
 const generateRound = () => {
   const number = getRandomNumber();
   const question = `${number}`;
-  let rightAnswer;
-
-  if (number % 2 === 0) {
-    rightAnswer = 'yes';
-  } else {
-    rightAnswer = 'no';
-  }
+  const rightAnswer = isEven(number);
 
   return [question, rightAnswer];
 };
